d0b82ca495f19070e37dfe23462efcd7eda921c0
g-burks <g.burks153@gmail.com>
2025-08-06 11:29:29 -0400

    .

--- backend/server.js
+++ backend/server.js
@@ -67,7 +67,7 @@ async function startServer() {
     /^https:\/\/.*\.loca\.lt$/
   ];
   app.use(cors({
-    origin: function (origin, callback) {
+    function (origin, callback) {
       console.log("CORS CHECK:", origin); // log every request
       if (!origin) return callback(null, true); // allow server-to-server or curl requests
       const isAllowed = allowedOrigins.some(o =>
